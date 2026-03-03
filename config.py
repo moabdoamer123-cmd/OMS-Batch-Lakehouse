2b5b1cd28388fd047c85f4c08772376f341d36a3
moabdoamer123-cmd <mo.abdo.amer123@gmail.com>
2026-03-03 03:23:27 +0200

    Update config.py: Add gold path and refactor S3_PATHS for dynamic table loading

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
 
-# إعدادات الويندوز
 def setup_windows_env():
     os.environ["HADOOP_HOME"] = r"C:\hadoop"
     os.environ["PATH"] += os.pathsep + r"C:\hadoop\bin"
 
-# إعدادات Spark & MinIO الموحدة
 SPARK_JARS = "jars/postgresql-42.7.3.jar,jars/hadoop-aws-3.3.4.jar,jars/aws-java-sdk-bundle-1.12.262.jar"
 MINIO_CONF = {
     "endpoint": "http://127.0.0.1:9000",
@@ -13,7 +11,6 @@ MINIO_CONF = {
     "secret_key": "password123"
 }
 
-# بيانات قاعدة البيانات (Postgres)
 DB_CONF = {
     "url": "jdbc:postgresql://ep-solitary-recipe-ah9qonat-pooler.c-3.us-east-1.aws.neon.tech/neondb",
     "user": "neondb_owner",
@@ -21,12 +18,10 @@ DB_CONF = {
     "driver": "org.postgresql.Driver"
 }
 
-# مسارات التخزين في الـ Data Lake
 S3_PATHS = {
-    "bronze": "s3a://bronze/orders/",
-    "silver": "s3a://silver/orders_partitioned/",
-    "gold_status": "s3a://gold/status_report/",
-    "gold_vip": "s3a://gold/top_customers/"
+    "bronze": "s3a://bronze",
+    "silver": "s3a://silver",
+    "gold": "s3a://gold"   
 }
 
 def get_spark_session(app_name):
@@ -45,13 +40,12 @@ def get_spark_session(app_name):
         .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
         .config("spark.hadoop.fs.s3a.fast.upload", "true") \
         .config("spark.hadoop.fs.s3a.fast.upload.buffer", "array") \
+        .config("spark.sql.adaptive.enabled", "true") \
         .getOrCreate()
 
-    # --- السطر السحري لحل مشكلة الويندوز (The Fix) ---
     try:
         spark._jvm.org.apache.hadoop.io.nativeio.NativeIO.Windows.access0 = lambda path, access: True
     except:
         pass
-    # -----------------------------------------------
 
     return spark
